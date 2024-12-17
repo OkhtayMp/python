839ceff90194300e02b90b336db1ff415dec8c03
Okhtay Mohammadpour <okhtaymp@proton.me>
2024-12-18 02:38:40 +0330

    Update apis_api.py
    Changed to 'object' to resolve 'Location' deserialization issues.

--- weatherapi/api/apis_api.py
+++ weatherapi/api/apis_api.py
@@ -988,7 +988,7 @@ class APIsApi(object):
             body=body_params,
             post_params=form_params,
             files=local_var_files,
-            response_type='Location',  # noqa: E501
+            response_type='object', # Changed to 'object' because 'Location' model deserialization was failing. # noqa: E501
             auth_settings=auth_settings,
             async_req=params.get('async_req'),
             _return_http_data_only=params.get('_return_http_data_only'),
